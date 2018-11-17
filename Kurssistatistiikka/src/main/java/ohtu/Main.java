415852777cba1b99a254eb364f8afefa7f908061
Etsku520 <eetu.huovila@gmail.com>
2018-11-17 17:58:37 +0200

    teht 3

--- Kurssistatistiikka/src/main/java/ohtu/Main.java
+++ Kurssistatistiikka/src/main/java/ohtu/Main.java
@@ -1,6 +1,8 @@
 package ohtu;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import java.io.IOException;
 import org.apache.http.client.fluent.Request;
 
@@ -15,9 +17,13 @@ public class Main {
 
         String url = "https://studies.cs.helsinki.fi/courses/students/"+studentNr+"/submissions";
         String urlC = "https://studies.cs.helsinki.fi/courses/courseinfo";
+        String urlOhtu = "https://studies.cs.helsinki.fi/courses/ohtu2018/stats";
+        String urlRuby = "https://studies.cs.helsinki.fi/courses/rails2018/stats";
 
         String bodyText = Request.Get(url).execute().returnContent().asString();
         String bodyTextC = Request.Get(urlC).execute().returnContent().asString();
+        String bodyTextOhtu = Request.Get(urlOhtu).execute().returnContent().asString();
+        String bodyTextRuby = Request.Get(urlRuby).execute().returnContent().asString();
         
 
         System.out.println("json-muotoinen data:");
@@ -27,6 +33,9 @@ public class Main {
         Gson mapper = new Gson();
         Submission[] subs = mapper.fromJson(bodyText, Submission[].class);
         Submission[] subsC = mapper.fromJson(bodyTextC, Submission[].class);
+        JsonParser parser = new JsonParser();
+        JsonObject parsittuDataOhtu = parser.parse(bodyTextOhtu).getAsJsonObject();
+        JsonObject parsittuDataRuby = parser.parse(bodyTextRuby).getAsJsonObject();
 
         for (Submission subc : subsC) {
             boolean found = false;
@@ -57,11 +66,23 @@ public class Main {
             }
             
             lause += "\nyhteensä: " + tasks + "/" + allex + " tehtävää " + hours + " tuntia\n\n";
+            if(subc.getName().contains("ohtu")) {
+                lause += "kursilla yhteensä " + parsittuDataOhtu.get("1").getAsJsonObject().get("students") + " palautusta, palautettuja tehtäviä " 
+                        + parsittuDataOhtu.get("1").getAsJsonObject().get("exercise_total") + " kpl, aikaa käytetty yhteensä " 
+                        + parsittuDataOhtu.get("1").getAsJsonObject().get("hour_total") + " tuntia" + "\n\n";
+            } else {
+                lause += "kursilla yhteensä " + parsittuDataRuby.get("1").getAsJsonObject().get("students") + " palautusta, palautettuja tehtäviä " 
+                        + parsittuDataRuby.get("1").getAsJsonObject().get("exercise_total") + " kpl, aikaa käytetty yhteensä " 
+                        + parsittuDataRuby.get("1").getAsJsonObject().get("hour_total") + " tuntia" + "\n\n";
+            }
+            
             
             if (found) {
                 System.out.println(lause);
             }
         }
+        
+        System.out.println(parsittuDataOhtu);
 
     }
 }
--- Kurssistatistiikka/src/main/java/ohtu/Submission.java
+++ Kurssistatistiikka/src/main/java/ohtu/Submission.java
@@ -9,10 +9,45 @@ public class Submission {
     private String name;
     private String term;
     private String year;
+    private int students;
+    private int hour_total;
+    private int exercise_total;
 
     public void setWeek(int week) {
         this.week = week;
     } 
+
+    public void setStudents(int students) {
+        this.students = students;
+    }
+
+    public void setHour_total(int hour_total) {
+        this.hour_total = hour_total;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setExercise_total(int exercise_total) {
+        this.exercise_total = exercise_total;
+    }
+
+    public int getStudents() {
+        return students;
+    }
+
+    public int getHour_total() {
+        return hour_total;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public int getExercise_total() {
+        return exercise_total;
+    }
 
     public String getYear() {
         return year;
